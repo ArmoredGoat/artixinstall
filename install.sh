07a67a2b0efd1a671e5b04272dfad4db36c7de2a
ArmoredGoat <social@armoredgoat.de>
2023-03-23 22:08:29 +0100

    Added remaining basic steps. First test version.

--- install.sh
+++ install.sh
@@ -13,7 +13,27 @@ read -n 1 -srp "Press any key to continue."
 printf "\nAvailable disks\n"
 lsblk --tree | grep 'NAME\|disk\|part'
 
-read -rp "Which disk shall be partitioned? " disk
+(lsblk --list -d | grep disk | awk '{print NR") /dev/" $1}') > /tmp/diskstemp
+
+while IFS= read -r line; do
+    echo $line
+done < /tmp/diskstemp
+
+linesnumber=$(wc -l < tempfile)
+
+while true
+do
+      read -rp "Which disk shall be partitioned? " disk
+      case $disk in
+            [1-$linesnumber])
+                  disk=$(sed "${disk}q;d" /tmp/diskstemp | awk '{print $2}')
+                  break
+                  ;;
+            *)
+                  echo "Invalid input. Please choose one of the available disks listed above."
+                  ;;
+      esac      
+done
 
 read -rp "Swap size in GB: " swap
 
@@ -22,11 +42,11 @@ do
       read -n 1 -rp "Do you want to perform a clean install? (y/N)" wipe
       case $wipe in
             [yY][eE][sS]|[yY])
-                  echo "Yes"
+                  wipe='y'
                   break
                   ;;
-            [nN][oO]|[nN])
-                  echo "No"
+            [nN][oO]|[nN]|"")
+                  wipe='n'
                   break
                   ;;
             *)
@@ -89,6 +109,11 @@ wipe=$(echo "$wipe" | tr '[:upper:]' '[:lower:]')
 username=$(echo "$username" | tr '[:upper:]' '[:lower:]')
 hostname=$(echo "$hostname" | tr '[:upper:]' '[:lower:]')
 
+disk0=$disk
+if [[ "$disk" == /dev/nvme0n* ]] || [[ "$disk" == /dev/mmcblk* ]]; then
+    disk="$disk"'p'
+fi
+
 # determine if running as UEFI or BIOS
 # If /sys/firmware/efi exists it is an UEFI boot
 if [ -d "/sys/firmware/efi" ]; then
@@ -97,6 +122,111 @@ else
     boot='bios'
 fi
 
+# start partitioning
+if [ "$boot" == 'uefi' ]; then
+    # gpt/uefi partitioning
+    if [ "$wipe" == y ]; then
+        partitions=0
+        wipefs --all --force "$disk0"
+        echo "g
+        n
+        1
+
+        +1024M
+        t
+        1
+        n
+        2
+
+        +8192M
+        t
+        2
+        19
+        n
+        3
+
+        
+        w
+        " | fdisk -w always -W always "$disk0"
+        #part 1 = boot, part 2 = swap, part 3 = root
+
+        # disk formatting
+        mkfs.fat -F 32 "$disk""$((1 + "$partitions"))"
+        fatlabel "$disk""$((1 + "$partitions"))" ESP
+        mkswap -L SWAP "$disk""$((2 + "$partitions"))"
+        mkfs.ext4 -L ROOT "$disk""$((3 + "$partitions"))"
+
+    else
+        partitions=$(lsblk "$disk0" -o NAME | grep -o '.$' | tail -1)
+        echo "n
+        +1024M
+        t
+        1
+        n
+        w
+        " | fdisk -W always "$disk0"
+    fi
+
+
+    # mounting storage and efi partitions
+    swapon /dev/disk/by-label/SWAP
+    mount /dev/disk/by-label/ROOT /mnt
+    mkdir /mnt/boot
+    mkdir /mnt/home
+    if [ "$boot" == 'uefi']; then
+        mkdir -p /mnt/{boot/EFI,etc/conf.d}
+        mount /dev/disk/by-label/ESP /mnt/boot/efi
+    fi
+    
+else
+    # mbr/bios partitioning
+    if [ "$wipe" == y ]; then
+        partitions=0
+        echo "o
+        n
+        p
+        w
+        " | fdisk -w always -W always "$disk0"
+    else
+        partitions=$(lsblk "$disk0" -o NAME | grep -o '.$' | tail -1)
+        echo "n
+        p
+        w
+        " | fdisk -W always "$disk0"
+    fi
+
+    # disk formatting
+    mkfs.ext4 -O fast_commit "$disk""$((1 + "$partitions"))"
+
+    # mounting storage (no efi partition, using dos label)
+    mount "$disk""$((1 + "$partitions"))" /mnt
+    mkdir -p /mnt/etc/conf.d
+fi
+
+fstabgen -U /mnt >> /mnt/etc/fstab
+
 # setting hostname
 echo "$hostname" > /mnt/etc/hostname
 echo "hostname=\'"$hostname"\'" > /mnt/etc/conf.d/hostname
+
+# installing base packages
+#base_devel='db diffutils gc guile libisl libmpc perl autoconf automake bash binutils bison esysusers etmpfiles fakeroot file findutils flex gawk gcc gettext grep groff gzip libtool m4 make pacman pacman-contrib patch pkgconf python sed opendoas texinfo which bc udev'
+basestrap /mnt base base_devel openrc elogind-openrc linux-lts linux-firmware git micro man-db bash-completion
+
+# exporting variables
+mkdir /mnt/tempfiles
+echo "$formfactor" > /mnt/tempfiles/formfactor
+echo "$cpu" > /mnt/tempfiles/cpu
+echo "$threadsminusone" > /mnt/tempfiles/threadsminusone
+echo "$gpu" > /mnt/tempfiles/gpu
+echo "$intel_vaapi_driver" > /mnt/tempfiles/intel_vaapi_driver
+echo "$boot" > /mnt/tempfiles/boot
+echo "$disk0" > /mnt/tempfiles/disk
+echo "$username" > /mnt/tempfiles/username
+echo "$password" > /mnt/tempfiles/userpassword
+echo "$rootpassword" > /mnt/tempfiles/rootpassword
+echo "$timezone" > /mnt/tempfiles/timezone
+
+#curl https://raw.githubusercontent.com/ArmoredGoat/artixinstall/main/chrootInstall.sh -o /mnt/chrootInstall.sh
+#chmod +x /mnt/chrootInstall.sh
+#artix-chroot /mnt /chrootInstall.sh
