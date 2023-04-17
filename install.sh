b0726c00e052a8d84ddc8efa51290886db5ccdd7
ArmoredGoat <ziegenhagen.julius@outlook.de>
2023-04-17 08:09:13 +0200

    Test other formating.

--- install.sh
+++ install.sh
@@ -587,15 +587,15 @@ if [ "$boot" == 'uefi' ]; then
 
 
     w
-    ' | fdisk -w always -W always $baseDisk
+    ' | fdisk -w always -W always "$baseDisk"
 
     # Format and label disks
-    mkfs.fat -F 32 $disk'1'
-    fatlabel $disk'1' ESP
+    mkfs.fat -F 32 "$disk"'1'
+    fatlabel "$disk"'1' ESP
     
-    mkswap -L SWAP $disk'2'
+    mkswap -L SWAP "$disk"'2'
     
-    mkfs.ext4 -L ROOT $disk'3'
+    mkfs.ext4 -L ROOT "$disk"'3'
         
     # Mount storage and EFI partitions, and create necessary directories
     swapon /dev/disk/by-label/SWAP
@@ -616,12 +616,12 @@ else
 
     -1M
     w
-    ' | fdisk -w always -W always $baseDisk
+    ' | fdisk -w always -W always "$baseDisk"
 
     # Format and label disks
-    mkswap -L SWAP $disk'1'
+    mkswap -L SWAP "$disk"'1'
     
-    mkfs.ext4 -L ROOT $disk'2'
+    mkfs.ext4 -L ROOT "$disk"'2'
 
     # Mount storage and EFI partitions, and create necessary directories
     swapon /dev/disk/by-label/SWAP
