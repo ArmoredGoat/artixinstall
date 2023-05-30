801c1ec4ef8a2fdd07d3291b4c1fa8255922fd4d
ArmoredGoat <social@armoredgoat.de>
2023-05-30 18:50:50 +0200

    Add add_service.

--- scripts/after_reboot.sh
+++ scripts/after_reboot.sh
@@ -11,6 +11,28 @@
 
 main () {
     install_displaylink
+    add_service displaylink
+}
+
+add_service () {
+    # Set service to given package/service
+    service="$1"
+    
+    # If no runlevel is given, set runlevel to default. Otherwise, use given
+    # runlevel.
+    if [[ ! "$2" ]]; then
+        runlevel="default"
+    else
+        runlevel="$2"
+    fi
+
+    # Add service to a runlevel to run it automatically if runlevel is met.
+    # TODO Explain runlevels
+    rc-update add $service $runlevel
+
+    # Start service now. Most of the time, the service is already running
+    # after installation but it never hurts to make sure.
+    rc-service $service start
 }
 
 install_packages () {
